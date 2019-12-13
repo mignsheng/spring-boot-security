083ecd6496be9b714a11d4117847f31469e248c6
mignsheng <273099274@qq.com>
2019-12-13 17:18:38 +0800

    1111

--- src/main/java/com/example/firstProject/config/security/BrowserSecurityConfig.java
+++ src/main/java/com/example/firstProject/config/security/BrowserSecurityConfig.java
@@ -6,6 +6,7 @@ import com.example.firstProject.config.imageValidate.ValidateCodeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.rememberme.PersistentToke
 import javax.sql.DataSource;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired MyAuthenticationSucessHandler authenticationSucessHandler;
